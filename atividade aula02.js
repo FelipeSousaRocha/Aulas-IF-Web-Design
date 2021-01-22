1d06ea4f4c0cf4b43a0e60b8fc854b8c8ef6586b
FelipeSousaRocha <77278366+FelipeSousaRocha@users.noreply.github.com>
2021-01-22 12:01:46 -0300

    Add files via upload

---
+++ atividade aula02.js
@@ -0,0 +1,11 @@
+/*var pergunta = confirm('Você está gostando do curso? ');
+if(pergunta == true)
+    document.write('Ficamos imensamente felizes que você esteja gostando!')
+else
+    prompt('Poxa! Em que podemos melhorar? ')*/
+
+function abreJanela()
+			{
+			   open('http://www.treinaweb.com.br');
+            }
+        
